80e4898f2f59a33f7b413421b45210fcb867f3dc
Graziani V Zafolin <zanfolin@gmail.com>
2025-08-06 11:55:56 -0300

    [feat] App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Projeto </Text>
+      <Text>Projeto 2B</Text>
       <StatusBar style="auto" />
     </View>
   );
