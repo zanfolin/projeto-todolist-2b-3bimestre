eb22aeed2c1e4023a2dbaf9a6a32e56352ec278b
Graziani V Zafolin <zanfolin@gmail.com>
2025-08-13 12:38:43 -0300

    [feat] App logo

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/icon_todo_list.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Projeto 2B</Text>
+      <View>
+        <Image source={logo} />
+      </View>
+      <Text>Projeto 2B - 12+1/08/2025</Text>
       <StatusBar style="auto" />
     </View>
   );
